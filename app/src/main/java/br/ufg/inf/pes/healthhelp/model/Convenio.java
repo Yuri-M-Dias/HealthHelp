723db5cbadc954c7d465af07a2f4b18ab7b8ae84
RenanOfugi <renangyn2010@hotmail.com>
2016-10-27 14:45:50 -0300

    Classes Modelos (Exceto atuação e agenda): Criados e implementados

---
+++ app/src/main/java/br/ufg/inf/pes/healthhelp/model/Convenio.java
@@ -0,0 +1,8 @@
+package br.ufg.inf.pes.healthhelp.model;
+
+/**
+ * Created by cintia on 27/10/16.
+ */
+
+public class Convenio {
+}
---
+++ app/src/main/java/br/ufg/inf/pes/healthhelp/model/enums/StatusProfissional.java
@@ -0,0 +1,8 @@
+package br.ufg.inf.pes.healthhelp.model.enums;
+
+/**
+ * Created by cintia on 27/10/16.
+ */
+
+public enum StatusProfissional {
+}
