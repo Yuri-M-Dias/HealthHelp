bec67af9e50ebfbc59e32b8f19dbe0dadccca308
Cleber de Souza Alcântara <cleber.93cd@gmail.com>
2016-11-08 21:58:21 -0200

    Renomeando activities da lista de locais de atendimento.

--- app/src/main/java/br/ufg/inf/pes/healthhelp/view/LocaisAtendimentoActivity.java
+++ app/src/main/java/br/ufg/inf/pes/healthhelp/view/LocaisAtendimentoActivity.java
@@ -33,7 +33,7 @@ public class LocaisAtendimentoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_lista_local_atendimento);
+        setContentView(R.layout.activity_locais_atendimento);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
