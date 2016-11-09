e805bd4313bc87fd20732bf199b56eb98338177b
RenanOfugi <renangyn2010@hotmail.com>
2016-11-09 02:26:05 -0200

    Elaboração do primeiro card

--- app/src/main/java/br/ufg/inf/pes/healthhelp/view/HomeActivity.java
+++ app/src/main/java/br/ufg/inf/pes/healthhelp/view/HomeActivity.java
@@ -1,8 +1,25 @@
 package br.ufg.inf.pes.healthhelp.view;
 
-/**
- * Created by Renan O on 06/11/2016.
- */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
-public class HomeActivity {
+import br.ufg.pes.healthhelp.R;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+
+    }
+
 }
