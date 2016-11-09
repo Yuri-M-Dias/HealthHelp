3458d02dc739e2baf8f7bbca24ebddc6f1f7eae6
RenanOfugi <renangyn2010@hotmail.com>
2016-11-09 03:11:51 -0200

    Finalizado metodos destinados aos cards da homeactivity

--- app/src/main/java/br/ufg/inf/pes/healthhelp/view/HomeActivity.java
+++ app/src/main/java/br/ufg/inf/pes/healthhelp/view/HomeActivity.java
@@ -3,6 +3,7 @@ package br.ufg.inf.pes.healthhelp.view;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import br.ufg.pes.healthhelp.R;
 
@@ -19,6 +20,26 @@ public class HomeActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+
+    }
+
+    public void OnClick$VisitasMedicas(View view){
+
+    }
+
+    public void OnClick$Agendar(View view){
+
+    }
+
+    public void OnClick$VisualizarAgenda(View view){
+
+    }
+
+    public void OnClick$VisualizarAtuacao(View view){
+
+    }
+
+    public void OnClick$LocaisAtendimento(View view){
 
     }
 
