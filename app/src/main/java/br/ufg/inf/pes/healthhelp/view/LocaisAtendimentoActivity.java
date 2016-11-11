8d585605de8edb6c382a3678814937d40a4916ec
Cleber de Souza Alcântara <cleber.93cd@gmail.com>
2016-11-11 01:48:52 -0200

    Adicionando função de voltar ao botão da activity de lista de locais.

--- app/src/main/java/br/ufg/inf/pes/healthhelp/view/LocaisAtendimentoActivity.java
+++ app/src/main/java/br/ufg/inf/pes/healthhelp/view/LocaisAtendimentoActivity.java
@@ -55,6 +55,17 @@ public class LocaisAtendimentoActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
         if(!Sessao.estaAtiva(this)) {
