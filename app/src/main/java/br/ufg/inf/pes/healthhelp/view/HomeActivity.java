14ef5cdbca541d7aff667674659cc8b1c8067f68
RenanOfugi <renangyn2010@hotmail.com>
2016-11-22 01:47:52 -0200

    Logout finalizado

--- app/src/main/java/br/ufg/inf/pes/healthhelp/view/HomeActivity.java
+++ app/src/main/java/br/ufg/inf/pes/healthhelp/view/HomeActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import br.ufg.inf.pes.healthhelp.model.Sessao;
 import br.ufg.inf.pes.healthhelp.service.MensagemNaoImplementado;
@@ -34,29 +33,29 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
 
-        if(!Sessao.estaAtiva(this)){
+        if (!Sessao.estaAtiva(this)) {
             Intent intent = new Intent(this, AutenticacaoActivity.class);
             startActivity(intent);
         }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.menu_homeactivity, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
 
             case R.id.logout:
-
+                logout(this);
                 return true;
 
             default:
@@ -64,38 +63,45 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
-    public void visualizarVisitasMedicas(View view){
+    public void visualizarVisitasMedicas(View view) {
 
         Context context = getApplicationContext();
         CharSequence text = "Visualizar visitas medicas nao implementado";
         MensagemNaoImplementado.MostraMensagemNaoImplementado(context, text);
     }
 
-    public void agendarVisitaMedica(View view){
+    public void agendarVisitaMedica(View view) {
 
         Context context = getApplicationContext();
         CharSequence text = "Agendar visitas medicas nao implementado";
         MensagemNaoImplementado.MostraMensagemNaoImplementado(context, text);
     }
 
-    public void visualizarAgendas(View view){
+    public void visualizarAgendas(View view) {
 
         Context context = getApplicationContext();
         CharSequence text = "Visualizar agenda nao implementado";
         MensagemNaoImplementado.MostraMensagemNaoImplementado(context, text);
     }
 
-    public void visualizarAtuacoes(View view){
+    public void visualizarAtuacoes(View view) {
 
         Context context = getApplicationContext();
         CharSequence text = "Visualizar atuação nao implementado";
         MensagemNaoImplementado.MostraMensagemNaoImplementado(context, text);
     }
 
-    public void visualizarLocaisAtendimento(View view){
+    public void visualizarLocaisAtendimento(View view) {
 
         Intent intent = new Intent(this, LocaisAtendimentoActivity.class);
         startActivity(intent);
     }
 
+    private void logout(Context context) {
+
+        Sessao.finalizarSessao(context);
+
+        Intent intent = new Intent(context, AutenticacaoActivity.class);
+        startActivity(intent);
+    }
 }
