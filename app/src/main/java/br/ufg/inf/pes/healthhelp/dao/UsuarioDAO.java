55d0adbf69461dd6a2adb3643d6a3a18d9153ed1
Yuri-M-Dias <yurimathe.yp@gmail.com>
2016-11-24 23:45:45 -0200

    Retira método desnecessário no UsuarioDAO

--- app/src/main/java/br/ufg/inf/pes/healthhelp/dao/UsuarioDAO.java
+++ app/src/main/java/br/ufg/inf/pes/healthhelp/dao/UsuarioDAO.java
@@ -4,16 +4,13 @@ import android.util.Log;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.util.AsyncExecutor;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 import br.ufg.inf.pes.healthhelp.model.Usuario;
 import br.ufg.inf.pes.healthhelp.model.event.DatabaseEvent;
@@ -25,70 +22,34 @@ public class UsuarioDAO extends AbstractDAO<Usuario> {
     }
 
     @Override
-    public void buscarTodos() {
+    public void buscarPelaId(String id) {
         getDatabaseReference()
             .child(DATABASE_CHILD)
+            .orderByChild("id")
+            .equalTo(id)
             .addListenerForSingleValueEvent(
                 new ValueEventListener() {
-                    List<Usuario> usuarios = new ArrayList<Usuario>();
-
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-                        Log.w(TAG, "buscarTodos.addValueListener: OnDataChange: Quantidade: " +
-                            dataSnapshot.getChildrenCount());
+                        GenericTypeIndicator<HashMap<String, Usuario>> genericTypeIndicator =
+                            new GenericTypeIndicator<HashMap<String, Usuario>>() { };
+                        HashMap<String, Usuario> usuarioHashMap = dataSnapshot.
+                            getValue(genericTypeIndicator);
+                        Usuario usuarioEncontrado = usuarioHashMap.values().iterator().next();
+                        Log.w(TAG, "buscaPeloId.OnDataChange: o usuário encontrado foi: " +
+                            usuarioEncontrado.getNome());
 
-                        for (DataSnapshot usuarioSnapshot : dataSnapshot.getChildren()) {
-                            Usuario usuarioRecebido = usuarioSnapshot.getValue(Usuario.class);
-                            usuarioRecebido.setId(usuarioSnapshot.getKey());
-                            usuarios.add(usuarioRecebido);
-                            Log.w(TAG, "Obtido: " + usuarios.get(usuarios.size() - 1).getId());
-                        }
-                        EventBus.getDefault().post(new DatabaseEvent<>(usuarios));
+                        // chama de volta quem pediu o local que foi buscado
+//                        getDatabaseCallback().onComplete(usuarioEncontrado);
                     }
 
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
-                        Log.e(TAG, "buscarTodos.setValueListener: onCancelled:", databaseError.
-                            toException());
-//                getDatabaseCallback().onError(databaseError);
-                    }
-                });
-    }
-
-    @Override
-    public void buscarPelaId(String id) {
-        getDatabaseReference().child(DATABASE_CHILD).orderByChild("id").equalTo(id).
-            addListenerForSingleValueEvent(new ValueEventListener() {
-                                               @Override
-                                               public void onDataChange(DataSnapshot dataSnapshot) {
-                                                   GenericTypeIndicator<HashMap<String, Usuario>> genericTypeIndicator =
-                                                       new GenericTypeIndicator<HashMap<String, Usuario>>() {
-                                                       };
-                                                   HashMap<String, Usuario> usuarioHashMap = dataSnapshot.
-                                                       getValue(genericTypeIndicator);
-                                                   Usuario usuarioEncontrado = usuarioHashMap.values().iterator().next();
-                                                   Log.w(TAG, "buscaPeloId.OnDataChange: o usuário encontrado foi: " +
-                                                       usuarioEncontrado.getNome());
-
-                                                   // chama de volta quem pediu o local que foi buscado
-//                        getDatabaseCallback().onComplete(usuarioEncontrado);
-                                               }
-
-                                               @Override
-                                               public void onCancelled(DatabaseError databaseError) {
-                                                   Log.e(TAG, "buscaPelaId: onCancelled:", databaseError.toException());
+                        Log.e(TAG, "buscaPelaId: onCancelled:", databaseError.toException());
 //                        getDatabaseCallback().onError(databaseError);
-                                               }
-                                           }
+                    }
+                }
             );
-    }
-
-    @Override
-    public void inserir(Usuario usuario) {
-        DatabaseReference registroUsuario = getDatabaseReference().child(DATABASE_CHILD).push();
-        Log.i(TAG, "Chave do novo usuario: " + registroUsuario.getKey());
-        registroUsuario.setValue(usuario);
-        usuario.setId(registroUsuario.getKey());
     }
 
     public void buscarPorLogin(final String login, final String senha) {
