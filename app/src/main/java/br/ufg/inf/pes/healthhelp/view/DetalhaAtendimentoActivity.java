4d728ebb140b37aa3be3d4746eedaf4d06238d0c
Jean Carlos <Jean Carlos>
2016-11-27 13:24:56 -0200

    Criacao tela detalhe de atendimentos

---
+++ app/src/main/java/br/ufg/inf/pes/healthhelp/view/DetalhaAtendimentoActivity.java
@@ -0,0 +1,16 @@
+package br.ufg.inf.pes.healthhelp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.ufg.pes.healthhelp.R;
+
+public class DetalhaAtendimentoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalha_atendimento);
+    }
+
+}
