561a835631434b427899da26ab266299a5deec4e
rodolphohiroshi <rodolpho.ht@hotmail.com>
2016-11-28 19:15:23 -0200

    Criação da VisitasMedicasActivity e layout activity_visitas_medicas V2 necessário criar implementação para simular a resposta do bd

--- app/src/main/java/br/ufg/inf/pes/healthhelp/view/VisitasMedicasActivity.java
+++ app/src/main/java/br/ufg/inf/pes/healthhelp/view/VisitasMedicasActivity.java
@@ -3,19 +3,14 @@ package br.ufg.inf.pes.healthhelp.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-
 import br.ufg.pes.healthhelp.R;
-
 
 public class VisitasMedicasActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visitas_medicas);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_visitas_medicas);
         setSupportActionBar(toolbar);
-
     }
 }
