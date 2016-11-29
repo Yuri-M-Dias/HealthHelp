d2cb109874f2c0416d2284a48bb9b04d75e4dc8f
Gleibson Silva <gleibsongyn@gmail.com>
2016-11-29 01:15:26 -0200

    Altera metodo que coleta data e horário de atendimento.

--- app/src/main/java/br/ufg/inf/pes/healthhelp/view/NovoAtendimentoActivity.java
+++ app/src/main/java/br/ufg/inf/pes/healthhelp/view/NovoAtendimentoActivity.java
@@ -1,23 +1,40 @@
 package br.ufg.inf.pes.healthhelp.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import br.ufg.inf.pes.healthhelp.model.Agenda;
+import br.ufg.inf.pes.healthhelp.model.Atendimento;
+import br.ufg.inf.pes.healthhelp.model.Atuacao;
+import br.ufg.inf.pes.healthhelp.model.PeriodoTempo;
+import br.ufg.inf.pes.healthhelp.model.enums.DayOfWeek;
 import br.ufg.inf.pes.healthhelp.service.LocalAtendimentoService;
 import br.ufg.pes.healthhelp.R;
 
+import static br.ufg.inf.pes.healthhelp.view.AgendaActivity.ARG_ATENDIMENTO_AGENDADO;
+
 public class NovoAtendimentoActivity extends AppCompatActivity {
 
-    private LocalAtendimentoService localAtendimentoService;
     private AutoCompleteTextView instituicao;
     private AutoCompleteTextView profissional;
-    private TextView dataHorario;
+    private Button dataHorario;
+    private Atendimento atendimento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +44,9 @@ public class NovoAtendimentoActivity extends AppCompatActivity {
 
         instituicao = (AutoCompleteTextView) findViewById(R.id.autoCompleteLocalAtendimento);
         profissional = (AutoCompleteTextView) findViewById(R.id.autoCompleteProfissionalSaude);
-        dataHorario = (TextView) findViewById(R.id.textView_dados_data_horario_atendimento);
+        dataHorario = (Button) findViewById(R.id.button_dados_data_horario_atendimento);
 
-        //essas variáveis serão instanciadas chamando o método: getNomesLocaisAtendimentos e getNomesProfissionais.
+        //essas variáveis serão instanciadas chamando os métodos: getNomesLocaisAtendimentos e getNomesProfissionais.
         String[] instituicoes = new String[] {"Hospital das Clínicas", "Hospinal Santa Genoveva",
             "Hospital HGG", "Hospital do Cancer", "Novo Hospital"};
         String[] profissionais = new String[] {"João não sei das quantas", "José Maria",
@@ -37,7 +54,7 @@ public class NovoAtendimentoActivity extends AppCompatActivity {
 
         instituicao.setAdapter(adapter(instituicoes));
         profissional.setAdapter(adapter(profissionais));
-        dataHorario.setText(getHorarioAtendimento());
+        dataHorario.setText(setHorarioAtendimento());
     }
 
     public ArrayAdapter<String> adapter(String[] listaDados){
@@ -47,29 +64,45 @@ public class NovoAtendimentoActivity extends AppCompatActivity {
     }
 
     public String[] getNomesLocaisAtendimentos(String dado){
-        //TODO buscar no banco todos os nomes dos locais para ser usado pelo adapter do AutoCompleteTextView
-        localAtendimentoService.solicitarListaLocaisAtendimento();
+        //buscar no banco todos os nomes dos locais para ser usado pelo adapter do AutoCompleteTextView
         return null;
     }
 
     public String[] getNomesProfissionais(String dado){
-        //TODO buscar no banco todos os nomes dos profissionais para ser usado pelo adapter do AutoCompleteTextView
+        //buscar no banco todos os nomes dos profissionais para ser usado pelo adapter do AutoCompleteTextView
         return null;
     }
 
-    public String getHorarioAtendimento(){
-        //TODO buscar o horário de atendimento do hospital/profissional selecionado nas AutoCompleteTestView
-        return "";
+    public void horarioAtendimento(View view){
+        Intent intent = new Intent(this, AgendaDisponivelActivity.class);
+        //intent.putExtra(AgendaActivity.ARG_ATUACAO, criarAtuacao());
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    public void startActivityForResult(Intent intent, int requestCode) {
+        super.startActivityForResult(intent, requestCode);
+        //Serializable at = intent.getSerializableExtra(ARG_ATENDIMENTO_AGENDADO);
+        //atendimento = ((Atendimento) at);
+
+    }
+
+    public String setHorarioAtendimento(){
+        //SimpleDateFormat data = new SimpleDateFormat("HH:mm");
+        //String horaData = "Das " + data.format(atendimento.getHoraInicio()) + " às " +
+         //   data.format(atendimento.getHoraFim());
+        return " ";
     }
 
     public void marcar(){
-        //TODO mandar salvar no banco o novo atendimento, este método é chamado no menu "Marcar"
+        CharSequence text = "Atendimento marcado com sucesso";
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+        finish();
     }
 
     public void initToolbar(){
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_novo_atendimento);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -97,6 +130,91 @@ public class NovoAtendimentoActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private Atuacao criarAtuacao() {
+        Atuacao atuacao = new Atuacao();
+        atuacao.setAgendas(new ArrayList<Agenda>());
+        Agenda agenda = new Agenda();
+        agenda.setNome("Atendimento Geral");
+        agenda.setId("id_agenda_atendimento_geral");
+        agenda.setHorariosBloqueados(new ArrayList<PeriodoTempo>());
+        agenda.setTempoPadraoMinutos(20);
+
+        List<PeriodoTempo> horariosAtendimento = new ArrayList<>();
+
+        PeriodoTempo periodo = new PeriodoTempo();
+        periodo.setDataInicio("25/11/2016");
+        periodo.setDataFim("25/12/2016");
+        periodo.setHoraInicio("08:00");
+        periodo.setHoraFim("12:00");
+        List<DayOfWeek> diasSemana = new ArrayList<>();
+        diasSemana.add(DayOfWeek.MONDAY);
+        diasSemana.add(DayOfWeek.TUESDAY);
+        diasSemana.add(DayOfWeek.WEDNESDAY);
+        diasSemana.add(DayOfWeek.THURSDAY);
+        diasSemana.add(DayOfWeek.FRIDAY);
+        periodo.setDiasSemana(diasSemana);
+        horariosAtendimento.add(periodo);
+
+        periodo = new PeriodoTempo();
+        periodo.setDataInicio("25/11/2016");
+        periodo.setDataFim("25/12/2016");
+        periodo.setHoraInicio("10:00");
+        periodo.setHoraFim("16:00");
+        diasSemana = new ArrayList<>();
+        diasSemana.add(DayOfWeek.SATURDAY);
+        periodo.setDiasSemana(diasSemana);
+        horariosAtendimento.add(periodo);
+
+        agenda.setHorariosAtendimento(horariosAtendimento);
+
+        atuacao.getAgendas().add(agenda);
+
+        agenda = new Agenda();
+        agenda.setNome("Cirurgias");
+        agenda.setId("id_agenda_cirurgias");
+        agenda.setHorariosBloqueados(new ArrayList<PeriodoTempo>());
+        agenda.setTempoPadraoMinutos(60);
+
+        horariosAtendimento = new ArrayList<>();
+
+        periodo = new PeriodoTempo();
+        periodo.setDataInicio("25/11/2016");
+        periodo.setDataFim("25/12/2016");
+        periodo.setHoraInicio("14:00");
+        periodo.setHoraFim("20:00");
+        diasSemana = new ArrayList<>();
+        diasSemana.add(DayOfWeek.MONDAY);
+        diasSemana.add(DayOfWeek.TUESDAY);
+        diasSemana.add(DayOfWeek.WEDNESDAY);
+        diasSemana.add(DayOfWeek.THURSDAY);
+        diasSemana.add(DayOfWeek.FRIDAY);
+        periodo.setDiasSemana(diasSemana);
+        horariosAtendimento.add(periodo);
+
+        atuacao.getAgendas().add(agenda);
+
+        PeriodoTempo periodoAlmoco = new PeriodoTempo();
+        periodoAlmoco.setHoraInicio(Calendar.getInstance());
+        periodoAlmoco.getHoraInicioCalendar().set(Calendar.HOUR_OF_DAY, 12);
+        periodoAlmoco.getHoraInicioCalendar().set(Calendar.MINUTE, 20);
+        periodoAlmoco.setHoraFim(Calendar.getInstance());
+        periodoAlmoco.getHoraFimCalendar().set(Calendar.HOUR_OF_DAY, 14);
+        periodoAlmoco.getHoraFimCalendar().set(Calendar.MINUTE, 00);
+        diasSemana = new ArrayList<>();
+        diasSemana.add(DayOfWeek.MONDAY);
+        diasSemana.add(DayOfWeek.TUESDAY);
+        diasSemana.add(DayOfWeek.WEDNESDAY);
+        diasSemana.add(DayOfWeek.THURSDAY);
+        diasSemana.add(DayOfWeek.FRIDAY);
+        diasSemana.add(DayOfWeek.SATURDAY);
+        periodoAlmoco.setDiasSemana(diasSemana);
+        atuacao.setHorariosAlmoco(new ArrayList<PeriodoTempo>());
+        atuacao.getHorariosAlmoco().add(periodoAlmoco);
+
+        agenda.setHorariosAtendimento(horariosAtendimento);
+        return atuacao;
     }
 
 }
